66b66137d368139fa5d8eff2f5c37c5452719ae8
Marc Milgrom <mdmilgrom@gmail.com>
2021-01-06 23:17:58 -0500

    Refactored as functions, added benchmark

--- Day1_1.jl
+++ Day1_1.jl
@@ -1,12 +1,22 @@
-list = map(x->tryparse(Int32,x),readlines("data/input_1_1.txt"))
-for e in list
-    for f in list
-        if f == e
-            continue
-        end
-        if in(2020 - e - f,list)
-            println(e * f * (2020-e-f))
-            break
+using BenchmarkTools
+list = map(x->tryparse(Int64,x),readlines("data/input_1_1.txt"))
+function solve1_1(list)::Int64
+    for e in list
+        if (2020 - e) ∈ list
+            return e * (2020-e)
         end
     end
 end
+
+function solve1_2(list)::Int64
+    for i in 1:length(list)
+        e = list[i]
+        for f in list[i+1:end]
+            (2020-e-f) ∈ list && return (2020-e-f) * e * f
+        end
+    end
+end
+@btime solve1_1(list)
+@btime solve1_2(list)
+println(solve1_1(list))
+println(solve1_2(list))
