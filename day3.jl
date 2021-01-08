cf7ab25e2c4a0e723c91faaad9385abf2fc3ee06
Marc Milgrom <mdmilgrom@gmail.com>
2021-01-07 23:34:17 -0500

    Refactor to function, add benchmark

--- day3.jl
+++ day3.jl
@@ -1,20 +1,22 @@
+using BenchmarkTools
 grid = readlines("data/input_3.txt")
-bounds = length(grid[1]),length(grid)
-slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
-trees = zeros(Int64,length(slopes)) 
-i = 1
-for slope in slopes
-    global i
-    local pos = [0,1]
-    #println("$slope $i")
-    while pos[2] + slope[2] <= bounds[2] 
-        @assert i < 6 "$i $pos $slope"
-        global trees
-        pos += slope
-        x = pos[1] % (bounds[1]) + 1
-        trees[i] += Int64(grid[pos[2]][x:x] == "#")
+function solve3(grid)
+    slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+    bounds = length(grid[1]),length(grid)
+    trees = zeros(Int64,length(slopes)) 
+    i = 1
+    for slope in slopes
+        pos = [0,1]
+        while pos[2] + slope[2] <= bounds[2] 
+            @assert i < 6 "$i $pos $slope"
+            pos += slope
+            x = pos[1] % (bounds[1]) + 1
+            trees[i] += Int64(grid[pos[2]][x:x] == "#")
+        end
+        i += 1
     end
-    i += 1
+    return trees
 end
-println(trees)
-println(prod(trees))
+@btime solve3(grid)
+println(solve3(grid))
+println(prod(solve3(grid)))
